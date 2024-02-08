e736d1d3998fed8e1a4673d50e28fc11595560c4
itaibiton <itaibiton@business.com>
2024-02-08 15:05:34 +0200

    added blank ui for login form instances

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,23 @@
+import Image from "next/image";
 import { LoginForm } from "@/features/login/LoginForm";
 import React from "react";
 function page() {
 	return (
 		<div className="w-full min-h-screen bg-background flex ">
-			<div className="w-full p-20 bg-card hidden md:flex">1</div>
-			<div className="w-full p-20 flex items-center justify-center">
+			<div className="w-full p-8 xl:p-20 bg-card hidden md:flex flex-col justify-between">
+				<Image
+					src="./cyfox.svg"
+					alt="logo"
+					width="200"
+					height="80"
+					className="h-14 scale-110"
+				/>
+				<p>
+					Best AI-Driven, End-to-End Cybersecurity Solutions for Your Business
+					Utilize machine learning to automatically detect cyber-attacks.
+				</p>
+			</div>
+			<div className="w-full p-20 flex justify-center">
 				<LoginForm />
 			</div>
 		</div>
--- features/login/LoginForm.tsx
+++ features/login/LoginForm.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import {
 	Form,
 	FormControl,
@@ -13,6 +14,9 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Facebook, Github, Loader2 } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import React, { useState } from "react";
 
 // Define the schema for email and password validation
 const formSchema = z.object({
@@ -20,7 +24,58 @@ const formSchema = z.object({
 	password: z.string().min(1, "Password fill in your password"), // Minimum 8 characters
 });
 
+type UserCredentials = {
+	email: string;
+	password: string;
+	isOTP: boolean;
+};
+
 export function LoginForm() {
+	const [userCredentials, setUserCredentials] = useState<UserCredentials>({
+		email: "",
+		password: "",
+		isOTP: false,
+	});
+
+	return (
+		<div className="flex flex-col w-full gap-4 rounded max-w-sm h-full py-52">
+			<div className="flex flex-col  w-full gap-4">
+				<Image
+					src="./cyfox.svg"
+					alt="logo"
+					width="200"
+					height="80"
+					className="h-14 scale-110 block md:hidden"
+				/>
+				<div className="flex flex-col gap-2">
+					<p className="text-xl font-medium">Login to Cyfox</p>
+					<p className="text-muted-foreground">
+						Enter your credentials below to sign in
+					</p>
+				</div>
+				<Separator />
+				{userCredentials.isOTP ? (
+					<OTPForm
+						userCredentials={userCredentials}
+						setUserCredentials={setUserCredentials}
+					/>
+				) : (
+					<CredentialsForm
+						userCredentials={userCredentials}
+						setUserCredentials={setUserCredentials}
+					/>
+				)}
+			</div>
+		</div>
+	);
+}
+
+type AuthForm = {
+	userCredentials: UserCredentials;
+	setUserCredentials: React.Dispatch<React.SetStateAction<UserCredentials>>;
+};
+
+const CredentialsForm = ({ userCredentials, setUserCredentials }: AuthForm) => {
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		mode: "all",
@@ -30,55 +85,86 @@ export function LoginForm() {
 		},
 	});
 
+	const [loading, setLoading] = useState(false);
+
 	function onSubmit(values: z.infer<typeof formSchema>) {
 		console.log(values);
+		setLoading(true);
 		// Handle login logic here
+		setTimeout(() => {
+			setLoading(false);
+			setUserCredentials((prev) => ({
+				email: values.email,
+				password: values.password,
+				isOTP: true,
+			}));
+		}, 2000);
 	}
 
 	return (
-		<Form {...form}>
-			<form
-				onSubmit={form.handleSubmit(onSubmit)}
-				className="border flex flex-col w-full p-4 gap-4 rounded"
-			>
-				<FormField
-					control={form.control}
-					name="email"
-					render={({ field, fieldState }) => (
-						<FormItem>
-							<FormLabel
-								className={`${
-									fieldState?.error?.message ? "text-red-500" : ""
-								}`}
-							>
-								{fieldState?.error?.message ?? "Email"}
-							</FormLabel>
-							<FormControl>
-								<Input type="email" placeholder="Your email" {...field} />
-							</FormControl>
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="password"
-					render={({ field, fieldState }) => (
-						<FormItem>
-							<FormLabel
-								className={`${
-									fieldState?.error?.message ? "text-red-500" : ""
-								}`}
-							>
-								{fieldState?.error?.message ?? "Password"}
-							</FormLabel>
-							<FormControl>
-								<Input type="password" placeholder="Your password" {...field} />
-							</FormControl>
-						</FormItem>
-					)}
-				/>
-				<Button type="submit">Login</Button>
-			</form>
-		</Form>
+		<div className="flex flex-col">
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="flex flex-col w-full gap-4"
+				>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field, fieldState }) => (
+							<FormItem>
+								<FormLabel
+									className={`${
+										fieldState?.error?.message ? "text-red-500" : ""
+									}`}
+								>
+									{fieldState?.error?.message ?? "Email"}
+								</FormLabel>
+								<FormControl>
+									<Input type="email" placeholder="Your email" {...field} />
+								</FormControl>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field, fieldState }) => (
+							<FormItem>
+								<FormLabel
+									className={`${
+										fieldState?.error?.message ? "text-red-500" : ""
+									}`}
+								>
+									{fieldState?.error?.message ?? "Password"}
+								</FormLabel>
+								<FormControl>
+									<Input
+										type="password"
+										placeholder="Your password"
+										{...field}
+									/>
+								</FormControl>
+							</FormItem>
+						)}
+					/>
+					<Button type="submit">
+						{loading ? <Loader2 className="w-5 h-5 animate-spin" /> : "Login"}
+					</Button>
+				</form>
+			</Form>
+			<div className="flex justify-evenly py-4">
+				<div className="">
+					<Facebook />
+				</div>
+				<div className="">
+					<Github />
+				</div>
+			</div>
+		</div>
 	);
-}
+};
+
+const OTPForm = ({ userCredentials, setUserCredentials }: AuthForm) => {
+	return <div>OTP Form</div>;
+};
