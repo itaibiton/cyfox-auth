c279038bbdb20cae077ddfffca344753e07a5702
itaibiton <itaibiton@business.com>
2024-02-11 01:46:43 +0200

    fix build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,6 @@
+import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
 
+const handler = NextAuth(authOptions);
 
-export const authOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.NEXT_PUBLIC_GITHUB_ID ?? "",
-            clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET ?? "",
-        }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text" },
-                uid: { label: "UID", type: "text" },
-            },
-            async authorize(credentials: any) {
-                // Here, you should validate the credentials (e.g., check if the UID and email match a user in your database)
-                console.log('here', credentials);
-                const user = { id: credentials?.uid, email: credentials?.email };
-                // Return null if the user cannot be found or credentials are invalid
-                if (!user) return null;
-                // Otherwise, return the user object
-                return user;
-            },
-        }),
-    ],
-};
-
-export const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
---
+++ utils/authOptions.ts
@@ -0,0 +1,29 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.NEXT_PUBLIC_GITHUB_ID ?? "",
+            clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET ?? "",
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                uid: { label: "UID", type: "text" },
+            },
+            async authorize(credentials: any) {
+                // Here, you should validate the credentials (e.g., check if the UID and email match a user in your database)
+                console.log('here', credentials);
+                const user = { id: credentials?.uid, email: credentials?.email };
+                // Return null if the user cannot be found or credentials are invalid
+                if (!user) return null;
+                // Otherwise, return the user object
+                return user;
+            },
+        }),
+    ],
+};
+
